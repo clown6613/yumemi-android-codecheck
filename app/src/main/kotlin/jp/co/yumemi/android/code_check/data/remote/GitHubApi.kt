94a20af2e7cb73b5296a0a3449494e50e95523f4
Kota Yoshikawa <clown.66137@gmail.com>
2023-05-01 14:58:27 +0900

    add: GitHubApi interface

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/remote/GitHubApi.kt
@@ -0,0 +1,11 @@
+package jp.co.yumemi.android.code_check.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface GitHubApi {
+    @Headers("application/vnd.github+json")
+    @GET("search/repositories")
+    suspend fun searchRepositories(@Query("q") query: String): SearchRepositoriesResultDto
+}
